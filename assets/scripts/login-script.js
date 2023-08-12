b3cecc5a5342b4a4564fbb8e1d1298fd672f9839
GedesumaWijaya <142064725+GedesumaWijaya@users.noreply.github.com>
2023-08-12 11:43:14 +0800

    Update login-script.js
    mengganti komentar

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,5 +1,5 @@
 /*
- * Catatan:
+ * Catatan
  * Gunakan fungsi goToHome() untuk menampilkan halaman home
  * Gunakan fungsi goToLogin() untuk menampilkan halaman login
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
